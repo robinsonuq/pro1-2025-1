0d847c78c76c4f57116410b1970e9005f75b8919
racionaldev <86496632+racionaldev@users.noreply.github.com>
2025-03-04 18:18:33 -0500

    update main

--- universidad/src/main/java/co/edu/uniquindio/poo/app/Main.java
+++ universidad/src/main/java/co/edu/uniquindio/poo/app/Main.java
@@ -7,19 +7,15 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
 
+
         Universidad universidad = new Universidad("Uiquindio", "Calle 12N");
 
-        String nombre = JOptionPane.showInputDialog(null,"Ingrese el nombre");
-        String identificacion = JOptionPane.showInputDialog(null,"Ingrese la identificacion");
-        String mensaje = universidad.registrarEstudiante(nombre,identificacion);
-        
-        JOptionPane.showMessageDialog(null,mensaje);
+        for (int i = 0; i < 5; i++) {
+            String nombre = JOptionPane.showInputDialog(null,"Ingrese el nombre");
+            String identificacion = JOptionPane.showInputDialog(null,"Ingrese la identificacion");
+            String mensaje = universidad.registrarEstudiante(nombre,identificacion);
+            JOptionPane.showMessageDialog(null,mensaje);
+        }
 
-         nombre = JOptionPane.showInputDialog(null,"Ingrese el nombre");
-         identificacion = JOptionPane.showInputDialog(null,"Ingrese la identificacion");
-
-         mensaje = universidad.registrarEstudiante(nombre,identificacion);
-
-        JOptionPane.showMessageDialog(null,mensaje);
     }
 }
