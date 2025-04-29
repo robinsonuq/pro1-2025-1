47afaa3a9d5986dedda59420356b3f918f68efd0
Robinson Arias <86636627+robinsonuq@users.noreply.github.com>
2025-04-29 09:52:38 -0500

    update empresa

---
+++ empresa/src/main/java/co/edu/uniquindio/poo/empresa/viewcontroller/PrimaryController.java
@@ -0,0 +1,34 @@
+package co.edu.uniquindio.poo.empresa.viewcontroller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import co.edu.uniquindio.poo.empresa.app.App;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class PrimaryController {
+    @FXML
+    private ResourceBundle resources;
+
+    App app;
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button primaryButton;
+
+    @FXML
+    void onOpenCrudCliente() {
+        app.openCrudCliente();
+    }
+
+    public void setApp(App app) {
+        this.app = app;
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+}
